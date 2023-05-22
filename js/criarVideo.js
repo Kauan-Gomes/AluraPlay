6b9d68c66fb405205692bb130507a6e95d9fe706
Kauan Gomes <Kauang13@gmail.com>
2023-05-22 16:10:01 -0300

    Api com Jscon server

---
+++ js/criarVideo.js
@@ -0,0 +1,11 @@
+const formulario = document.querySelector("[data-formulario]")
+
+function criarVideo(evento){
+    evento.preventDefault()
+    const imagem = document.querySelector("[data-imagem]").value
+    const url = document.querySelector("[data-url]").value
+    const titulo = document.querySelector("[data-titulo]").value
+    const descricao = math.floor(math.random() *10).toString()
+}
+
+formulario.addEventListener("submit", evento => criarVideo(evento))
