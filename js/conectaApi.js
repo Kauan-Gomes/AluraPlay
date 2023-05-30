500eed6df93be7a51fd5758a2fb21e58306ff902
Kauan Gomes <Kauang13@gmail.com>
2023-05-30 14:55:57 -0300

    codigo finalizado

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -6,24 +6,42 @@ async function listaVideos() {
 }
 
 async function criaVideo(titulo, descricao, url, imagem) {
-    const conexao = await fetch("http:/localhost:3000/videos", {
+    const conexao = await fetch("http://localhost:3000/videos", {
         method: "POST",
         headers: {
             "Content-type": "application/json"
         },
         body: JSON.stringify({
             titulo: titulo,
-            descricao: `${descricao} mil  visualizações`,
+            descricao: `${descricao} mil visualizações`,
             url: url,
             imagem: imagem
         })
-    })
+    });
 
-    const conexaoConvertida = awaitconexao.json()
-    return conexaoConvertida;
+    if (!conexao.ok) {
+        throw new Error("Erro ao enviar vídeo");
+    }
+
+    const resposta = await conexao.text();
+
+    try {
+        const conexaoConvertida = JSON.parse(resposta);
+        return conexaoConvertida;
+    } catch (error) {
+        throw new Error("Erro ao processar resposta");
+    }
 }
+
+async function buscaVideo(termobusca){
+    const conexao = await fetch (`http://localhost:3000/videos?q=${termobusca}`)
+    const conexaoConvertida = conexao.json()
+
+    return conexaoConvertida;
+}   
 
 export const conectaApi = {
     listaVideos, 
-    criaVideo
+    criaVideo,
+    buscaVideo
 }
